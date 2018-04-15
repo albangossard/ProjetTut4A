03f53070f8665884d127cd4b164267381f7069fe
Alban Gossard <alban.gossard@hotmail.fr>
2018-04-15 11:08:08 +0200

    comparaison indices Sobol indep

---
+++ Modele_2D/compareSobol.py
@@ -0,0 +1,25 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+list_choice=[0,1,2]
+
+list_x=np.array([437454, 425697, 412291])/10000.
+list_sobolKs=[]
+list_sobolQ=[]
+for choice in list_choice:
+    with open('sensAnalysis_choice='+str(choice)+'/uqK/sensitivity.json') as jsonData:
+        data=json.load(jsonData)
+        list_sobolKs.append(data['S_T_Ks'][0][0])
+        list_sobolQ.append(data['S_T_Q'][0][0])
+print("list_x="+str(list_x))
+print("list_sobolKs="+str(list_sobolKs))
+print("list_sobolQ="+str(list_sobolQ))
+
+plt.scatter(list_x, list_sobolKs, marker='o', label='Ks')
+plt.scatter(list_x, list_sobolQ, marker='*', label='Q')
+plt.xlabel('x (km)')
+plt.ylabel('Sobol total indices')
+plt.legend()
+plt.savefig('Sobol_indep_compare_3pts.png', dpi=200)
+plt.show()

6264b57934ef7ebf678226b7d02d7808f861fb5c
Alban Gossard <alban.gossard@hotmail.fr>
2018-05-02 13:29:16 +0200

    test pyplot sur gmmcalcul

--- Modele_2D/errorInFunctionOfWaterLevel.py
+++ Modele_2D/errorInFunctionOfWaterLevel.py
@@ -1,19 +1,28 @@
+import sys
+import matplotlib
+# matplotlib.use('GTK')
+print(matplotlib.get_backend())
 from Substitut import *
+
+options = [sys.argv[i+1] for i in range(len(sys.argv)-1)]
+
+if 'k' in options:
+    method = 'krig'
+if 'pc' in options:
+    method = 'pc'
 
 list_choice=[0,1,2]
 gamme=-1
-seuil_Q_1=3000.
-seuil_Q_2=6000.
-dists=['Uniform(17., 45.)','Normal(5750., 2075.)']; distribName='Norm'
-# dists=['Uniform(17., 45.)','Uniform(1600., 9900.)']; distribName='Unif'
+distribName='Norm'
+# distribName='Unif'
 
 for choice in list_choice:
     if gamme==-1:
-        list_h_pred_krig = np.loadtxt('postProcessingData/LOO_list_h_pred_krig_'+distribName+'_choice='+str(choice)+'.txt')
-        list_h_val_krig = np.loadtxt('postProcessingData/LOO_list_h_val_krig_'+distribName+'_choice='+str(choice)+'.txt')
+        list_h_pred_krig = np.loadtxt('postProcessingData/LOO_list_h_pred_'+method+'_'+distribName+'_choice='+str(choice)+'.txt')
+        list_h_val_krig = np.loadtxt('postProcessingData/LOO_list_h_val_'+method+'_'+distribName+'_choice='+str(choice)+'.txt')
     else:
-        list_h_pred_krig = np.loadtxt('postProcessingData/LOO_list_h_pred_krig_'+distribName+'_gamme='+str(gamme)+'_choice='+str(choice)+'.txt')
-        list_h_val_krig = np.loadtxt('postProcessingData/LOO_list_h_val_krig_'+distribName+'_gamme='+str(gamme)+'_choice='+str(choice)+'.txt')
+        list_h_pred_krig = np.loadtxt('postProcessingData/LOO_list_h_pred_'+method+'_'+distribName+'_gamme='+str(gamme)+'_choice='+str(choice)+'.txt')
+        list_h_val_krig = np.loadtxt('postProcessingData/LOO_list_h_val_'+method+'_'+distribName+'_gamme='+str(gamme)+'_choice='+str(choice)+'.txt')
     err = np.abs(list_h_val_krig-list_h_pred_krig)
     plt.scatter(list_h_val_krig, err)
     plt.show()

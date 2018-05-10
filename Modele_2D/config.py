07b294b2fc779ecb5ffb09d0f172cdc0bb459cd7
Alban Gossard <alban.gossard@hotmail.fr>
2018-05-10 23:25:04 +0200

    pret pour simul à degree=4

--- Modele_2D/config.py
+++ Modele_2D/config.py
@@ -11,8 +11,8 @@ corner_gamme0 = ([17.,1600.],[45.,seuil_Q_1])
 corner_gamme1 = ([17.,seuil_Q_1],[45.,seuil_Q_2])
 corner_gamme2 = ([17.,seuil_Q_2],[45.,9900.])
 
-# degree_default = 6*0+4
-degree_default = 6
+degree_default = 6*0+4
+# degree_default = 6
 nb_pts_generation_distrib = 10000
 list_x=np.array([22., 36., 62.])
 
